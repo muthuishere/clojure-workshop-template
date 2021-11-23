27dcfdaff832e98a853d5a08cf465c244825ccd3
Muthukumaran <muthuishere@gmail.com>
2021-11-23 16:16:25 +0530

    Updated Code base

--- workspace/src/hello_clojure/core.clj
+++ workspace/src/hello_clojure/core.clj
@@ -1,7 +1,17 @@
 (ns hello_clojure.core)
 
 
-;Right Click this File And Select Start or COnnect to Clojure REPL
+      ; 1 => Right Click 
+
+      ; 2 => Select Start or Connect to Clojure REPL
+
+      ; 3 => Choose  : Start Your Project with REPL and connect (a.k.a Jackin)
+
+      ; 4 =>Choose :   Leiningen
+
+      ; Finally you should see a repl on right side
+
+
 "Hello Clojure"
 
 (+ 2 3)
