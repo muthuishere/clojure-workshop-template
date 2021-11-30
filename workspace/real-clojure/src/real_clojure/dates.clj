6548077052c1b6e5cc64bd902b0cfc910357d907
Muthukumaran Navaneethakrishnan <muthuishere@gmail.com>
2021-11-30 15:31:22 +0000

    updated

--- workspace/real-clojure/src/real_clojure/dates.clj
+++ workspace/real-clojure/src/real_clojure/dates.clj
@@ -21,4 +21,54 @@
   
   )
 
+
+(comment 
+
+
+  
 ;(add 2 3)
+
+(drop 200 (range 300))
+
+
+(drop 1 (take 2 [34 21 75]))
+
+
+(drop 5 (take 10 (range)))
+
+  (defn is-less-than-20? [n]
+    (< n 20))
+  
+  
+  ; List<Long> items =[]
+  ;for(Long n :numbers){
+  ;  if(n < 20){
+  ;  break
+  ;else
+  ;  items.add(n)                                                 ;
+  ; }
+(take-while is-less-than-20? [34 56 7 8 12])
+
+(take-while is-less-than-20? [7 8 12 34 56])
+
+
+(take-while is-less-than-20? [7 8 34 12 56])
+
+
+(take-while (fn [n]
+              (< n 20)) [7 8 34 12 56])
+
+(take-while #(< % 20) [7 8 34 12 56])
+
+
+
+; Home Assignment
+;In Array
+["Spider Man" "Iron Man" "Captain America"]
+
+  ; drop-while , if the string contains Man
+;  Result("Captain America")
+
+
+
+)
