cee566a3ac5ee58fb4963d4aa9734d1f292794e0
Muthukumaran <muthuishere@gmail.com>
2021-12-02 21:04:37 +0530

    updated with home assignment

--- workspace/real-clojure/src/real_clojure/destructure/destrcuter_map.clj
+++ workspace/real-clojure/src/real_clojure/destructure/destrcuter_map.clj
@@ -7,3 +7,24 @@
 ;
 ;  Name : Macbook  Brand Apple
 ;  Total  : 90000 x 2 = 180000
+
+
+(defn print-invoice [{:keys [name price manufacturer quantity] }]
+
+
+
+    (println (str " Name : "  name " Brand " manufacturer))
+    (println (str "Total  : " price " x " quantity "="  (* price quantity)))
+
+
+
+  )
+
+(print-invoice product)
+
+;Home Assignment
+
+(def spider-man {:name "Peter Parker" :alias "Spider Man" :mentor { :name "Tony Stark" :alias "Iron Man"}})
+;create a method to print Mentor name
+
+
