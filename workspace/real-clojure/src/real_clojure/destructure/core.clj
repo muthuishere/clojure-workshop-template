8324e18e7017a3e5c3b8b6a73c1899627fbfd33f
Muthukumaran Navaneethakrishnan <muthuishere@gmail.com>
2021-12-02 15:35:31 +0000

    committed

---
+++ workspace/real-clojure/src/real_clojure/destructure/core.clj
@@ -0,0 +1,9 @@
+(ns real-clojure.destructure.core)
+
+;Exercise
+(def super-hero-data ["Peter Parker" "Spider Man"])
+
+;Create a method which accepts a variable , The Method should Print
+;
+;  "Real Name : Peter Parker"
+;  "Alias : Spider Man"
